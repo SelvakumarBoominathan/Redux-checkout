affc9e1e90084e639e45d123c9509c9ffe52da22
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-01 01:28:10 +0530

    added store

---
+++ src/store.js
@@ -0,0 +1,9 @@
+// import { configureStore } from "@reduxjs/toolkit";
+
+import { createStore } from redux;
+
+import costReducer from "./reducer"
+
+const store = createStore(costReducer);
+
+export default store;
