55a2b20ae9e76198771f0aff716007ae374e3854
Selvakumar Boominathan <selvans2k@gmail.com>
2024-05-31 23:53:05 +0530

    changes

--- src/reducer/total.js
+++ src/reducer/total.js
@@ -1,3 +1,13 @@
 const initialState = {
-  
-}
+  total: 0,
+};
+
+const totalReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "inc":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+  }
+};
